e1f582e816cfb302ca4d3cb6120689ec041d533a
Gabriel Gouveia <bielsantos30@hotmail.com>
2023-03-16 13:50:04 -0300

    Gabriel: balta 16/03

--- Program.cs
+++ Program.cs
@@ -7,6 +7,20 @@ namespace Balta// Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            const int idadeMinima = 18;
+            
+            bool aoba = false;
+
+            object quantidade;
+
+            int? idade = null;
+
+            int x = -20;
+            uint y = (uint) x;
+
+            Console.WriteLine(y + " " + y.GetType());   
+            
+            
         }
     }
 }

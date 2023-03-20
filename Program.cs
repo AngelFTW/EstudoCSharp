941190602a7798538622165c60f2f7cfcc8c9036
Gabriel Gouveia <bielsantos30@hotmail.com>
2023-03-20 15:17:20 -0300

    Gabriel: Balta 20/03

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace Balta// Note: actual namespace depends on the project name.
             Console.WriteLine("Hello World!");
             const int idadeMinima = 18;
             
-            bool aoba = false;
+            bool aopa = false;
 
             object quantidade;
 
@@ -20,7 +20,39 @@ namespace Balta// Note: actual namespace depends on the project name.
 
             Console.WriteLine(y + " " + y.GetType());
             
+            var valor = 0;
+
+            do{
+                Console.WriteLine("Opa");
+                valor++;
+            } while(valor < 5);
+
+            aoba("oi meu chapa");
+
+            var nomesob = nome("Gab", "Gouveia");
             
+
+            var arr = new string[2];
+            arr[0] = "item 1";
+
+            var arr2 = arr;
+
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr2[0]);
+
+            arr[0] = "item 2";
+
+            
+            Console.WriteLine(arr[0]);            
+            Console.WriteLine(arr2[0]);
+        }
+
+        static void aoba (string par){
+            Console.WriteLine(par);
+        }
+
+        static string nome(string nome, string sobrenome) {
+            return nome + " " + sobrenome;
         }
     }
 }

0d397a795e376d66da9964a6d0120587ed67b157
Gabriel Gouveia <bielsantos30@hotmail.com>
2023-03-21 09:04:06 -0300

    Gabriel: Balta 21/03

--- Program.cs
+++ Program.cs
@@ -45,6 +45,36 @@ namespace Balta// Note: actual namespace depends on the project name.
             
             Console.WriteLine(arr[0]);            
             Console.WriteLine(arr2[0]);
+
+            Product mouse = new Product(1, "Mouse Gamer", 199.99, EProductType.Product);
+            var manuntencaoEletrica = new Product(2,"Manuntencao eletrica", 500.00, EProductType.Service);
+
+            Console.WriteLine(mouse.Id);            
+            Console.WriteLine(mouse.Name);
+            Console.WriteLine(mouse.Price);
+            Console.WriteLine(mouse.Type);
+            Console.WriteLine((int)mouse.Type);
+        }
+
+        struct Product{
+
+            public Product(int id, string name, double price, EProductType type){
+
+                Id = id;
+                Name = name;
+                Price = price;
+                Type = type;
+
+            }
+
+            public int Id;
+            public string Name;
+            public double Price;
+            public EProductType Type;
+
+            public double PriceInDolar(double dolar){
+                return Price * dolar;
+            }
         }
 
         static void aoba (string par){
@@ -53,6 +83,11 @@ namespace Balta// Note: actual namespace depends on the project name.
 
         static string nome(string nome, string sobrenome) {
             return nome + " " + sobrenome;
+        }
+
+        enum EProductType{
+            Product = 1,
+            Service = 2
         }
     }
 }

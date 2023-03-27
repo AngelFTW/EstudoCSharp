7831c74724af84037dca86ef429c6eab7f1faa1b
Gabriel Gouveia <bielsantos30@hotmail.com>
2023-03-27 10:20:54 -0300

    Gabriel: Commit inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EditorHTML // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        public static class Menu
+        {
+            
+        }
+    }
+}

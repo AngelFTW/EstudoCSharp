85d04a968901a71db4b4da6c93485ff49a1e486c
Gabriel Gouveia <bielsantos30@hotmail.com>
2023-04-05 13:47:30 -0300

    Gabriel: Balta Moedas Completo

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Moedas // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var valor = 10.25;
+            decimal valor1 = 10.25m;
+            Console.WriteLine(Math.Round(valor));
+            Console.WriteLine(Math.Ceiling(valor));
+            Console.WriteLine(Math.Floor(valor));
+
+        }
+    }
+}

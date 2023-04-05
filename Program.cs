5f1c98927dc19eefd782dcb7118b846fafb38ef0
Gabriel Gouveia <bielsantos30@hotmail.com>
2023-04-05 15:10:37 -0300

    Gabriel: Arrays Balta 05/04

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Arrays // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var meuArray = new int[5];
+
+            foreach (var item in meuArray)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.WriteLine("\n");
+
+
+            var funcionarios = new Funcionario[5];
+            funcionarios[0] = new Funcionario { Id = 1, Nome = "João" };
+
+            foreach (var funcionario in funcionarios)
+            {
+                Console.WriteLine(funcionario.Id);
+            }
+        }
+
+        public struct Funcionario
+        {
+            public int Id{ get; set; }
+            public string Nome { get; set; }
+        }
+    }
+}

de8226ae4566fd496e9ee9d8e85f345ae5079a22
Gabriel Gouveia <bielsantos30@hotmail.com>
2023-04-06 08:44:29 -0300

    Gabriel: Balta Arrays Completo

--- Program.cs
+++ Program.cs
@@ -24,6 +24,15 @@ namespace Arrays // Note: actual namespace depends on the project name.
             {
                 Console.WriteLine(funcionario.Id);
             }
+
+            var arr = new int[4];
+            var arrb = arr; // São a mesma coisa, arr e arrb apontam pro mesmo lugar na memória.
+
+            var arr1 = new int[4];
+            var arr2 = new int[4];
+
+            arr1[0] = arr2[0];
+            arr1.CopyTo(arr2, 0); // Copia o conteúdo de arr1 para arr2.
         }
 
         public struct Funcionario
